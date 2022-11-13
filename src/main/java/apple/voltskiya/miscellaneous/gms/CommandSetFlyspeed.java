81c9d99cb18308baa8acad01a15b9295ec5eb3c0
amp7368 <47309189+amp7368@users.noreply.github.com>
2022-11-12 19:16:42 -0500

    Removed AppleBiomeEdit remenants in favor of a new plugin

--- src/main/java/apple/voltskiya/miscellaneous/gms/CommandSetFlyspeed.java
+++ src/main/java/apple/voltskiya/miscellaneous/gms/CommandSetFlyspeed.java
@@ -1,11 +1,11 @@
 package apple.voltskiya.miscellaneous.gms;
 
-import apple.voltskiya.miscellaneous.VoltskiyaPlugin;
 import apple.lib.acf.BaseCommand;
 import apple.lib.acf.annotation.CommandAlias;
 import apple.lib.acf.annotation.CommandCompletion;
 import apple.lib.acf.annotation.CommandPermission;
 import apple.lib.acf.annotation.Default;
+import apple.voltskiya.miscellaneous.VoltskiyaPlugin;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -18,7 +18,7 @@ public class CommandSetFlyspeed extends BaseCommand {
     }
 
     @Default
-    @CommandCompletion("speed @range:10")
+    @CommandCompletion("@range:10")
     public void flySpeed(Player player, float speed) {
         if (speed > 10 || speed < 0) {
             player.sendMessage(ChatColor.RED + "The speed must be between 0 and 10");
