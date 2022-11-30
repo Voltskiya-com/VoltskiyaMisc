7b4da0c4a9ab4200c08cf6ae1dd4893954f1d353
VanicArts <mattis.margenberg2med.de>
2022-11-30 15:22:46 +0100

    recipes are discovered when joined

--- src/main/java/com/voltskiya/misc/players/crafting/RegisterCrafting.java
+++ src/main/java/com/voltskiya/misc/players/crafting/RegisterCrafting.java
@@ -7,12 +7,17 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.ShapedRecipe;
 
-public class RegisterCrafting {
+public class RegisterCrafting implements Listener {
+    ArrayList<NamespacedKey> recipesNames = new ArrayList<>();
 
     public RegisterCrafting() {
         recipeFilter();
@@ -26,33 +31,39 @@ public class RegisterCrafting {
         recipe.shape("N", "I", "N");
         recipe.setIngredient('I', Material.IRON_INGOT);
         recipe.setIngredient('N', Material.IRON_NUGGET);
+        recipesNames.add(recipe.getKey());
         Bukkit.addRecipe(recipe);
     }
     private void recipeWool(){
         ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(VoltskiyaPlugin.get(), "Wool"), new ItemStack(Material.STRING, 4));
         recipe.shape("W");
         recipe.setIngredient('W', new RecipeChoice.MaterialChoice(woolList()));
+        recipesNames.add(recipe.getKey());
         Bukkit.addRecipe(recipe);
     }
     private void recipeChainArmor(){
         ShapedRecipe helmet = new ShapedRecipe(new NamespacedKey(VoltskiyaPlugin.get(), "ChainHelmet"), new ItemStack(Material.CHAINMAIL_HELMET, 1));
         helmet.shape("CCC", "C C");
         helmet.setIngredient('C', Material.CHAIN);
+        recipesNames.add(helmet.getKey());
         Bukkit.addRecipe(helmet);
 
         ShapedRecipe chestPlate = new ShapedRecipe(new NamespacedKey(VoltskiyaPlugin.get(), "ChainChestPlate"), new ItemStack(Material.CHAINMAIL_CHESTPLATE, 1));
         chestPlate.shape("C C", "CCC", "CCC");
         chestPlate.setIngredient('C', Material.CHAIN);
+        recipesNames.add(chestPlate.getKey());
         Bukkit.addRecipe(chestPlate);
 
         ShapedRecipe leggings = new ShapedRecipe(new NamespacedKey(VoltskiyaPlugin.get(), "ChainLeggings"), new ItemStack(Material.CHAINMAIL_LEGGINGS, 1));
         leggings.shape("CCC", "C C", "C C");
         leggings.setIngredient('C', Material.CHAIN);
+        recipesNames.add(leggings.getKey());
         Bukkit.addRecipe(leggings);
 
         ShapedRecipe boots = new ShapedRecipe(new NamespacedKey(VoltskiyaPlugin.get(), "ChainBoots"), new ItemStack(Material.CHAINMAIL_BOOTS, 1));
         boots.shape("C C", "C C");
         boots.setIngredient('C', Material.CHAIN);
+        recipesNames.add(boots.getKey());
         Bukkit.addRecipe(boots);
     }
 
@@ -87,6 +98,11 @@ public class RegisterCrafting {
         choices.add(Material.RED_WOOL);
         choices.add(Material.BLACK_WOOL);
         return choices;
+    }
+    @EventHandler
+    private void onPlayerJoinAddRecipes(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.discoverRecipes(recipesNames);
     }
 
 }
